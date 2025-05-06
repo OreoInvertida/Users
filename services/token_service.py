9cf5128ba80b742aa818add907d1ffdb4c6a6718
DamianDuque <damianduquel@gmail.com>
2025-05-06 13:42:49 -0500

    feat: JWT token management

---
+++ services/token_service.py
@@ -0,0 +1,27 @@
+# auth/services/token_service.py
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from fastapi import HTTPException, status, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("JWT_SECRET", "super-secret-key")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+security = HTTPBearer()
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload  # puedes retornar el email o cualquier info
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token inválido o expirado",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
