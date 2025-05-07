709c94cd5317b77c75df329ea4d42e3db15f632b
DamianDuque <damianduquel@gmail.com>
2025-05-07 06:51:06 -0500

    update: deployed db's

--- database/mongodb.py
+++ database/mongodb.py
@@ -1,5 +1,9 @@
 # users/database/mongodb.py
 from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://oreo:negronauta@users.nppj2o.mongodb.net/?retryWrites=true&w=majority&appName=Users")
+DB_NAME = os.getenv("AUTH_DB_NAME", "auth_db")
 
 class MongoDB:
     client = None
@@ -8,6 +12,6 @@ class MongoDB:
 mongo = MongoDB()
 
 async def connect_to_mongo():
-    mongo.client = AsyncIOMotorClient("mongodb://localhost:27017")
+    mongo.client = AsyncIOMotorClient(MONGO_URI)
     mongo.db = mongo.client["carpeta_ciudadana"]
     print("Conexión iniciada.")
