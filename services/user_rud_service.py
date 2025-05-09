7b3784f01334ba8853ce894090008d06d8a5ca57
DamianDuque <damianduquel@gmail.com>
2025-05-09 12:42:03 -0500

    fix: unique email verification

--- services/user_rud_service.py
+++ services/user_rud_service.py
@@ -5,12 +5,12 @@ from models.user_model import UserUpdate
 
 async def get_user_by_id(user_id: int):
     collection = mongo.db["users"]
-    user = await collection.find_one({"user_id": user_id})
+    user = await collection.find_one({"id": user_id})
     if not user:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     
     user.pop("_id")
-    user["user_id"] = str(user["user_id"])
+    user["id"] = str(user["id"])
     return user
 
 async def get_user_by_email(user_email: str):
@@ -24,14 +24,14 @@ async def get_user_by_email(user_email: str):
 async def update_user(user_id: int, updates: UserUpdate):
     collection = mongo.db["users"]
     data = {k: v for k, v in updates.dict().items() if v is not None}
-    result = await collection.update_one({"user_id": user_id}, {"$set": data})
+    result = await collection.update_one({"id": user_id}, {"$set": data})
     if result.matched_count == 0:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return {"message": "Usuario actualizado exitosamente."}
 
 async def delete_user(user_id: int):
     collection = mongo.db["users"]
-    result = await collection.delete_one({"user_id": user_id})
+    result = await collection.delete_one({"id": user_id})
     if result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return {"message": "Usuario eliminado exitosamente."}
